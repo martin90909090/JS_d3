8b4792d30eaa764989ac74306412eb793fd0fde1
martin90909090 <personalmperez@gmail.com>
2023-12-24 21:06:40 -0300

    m

--- assets/JS/script2.js
+++ assets/JS/script2.js
@@ -1,20 +1,38 @@
-let color;
+let color1, color2;
 
-window.addEventListener('keydown', (event) => {
+const createDiv = (color2 = "white") => {
+    const div = document.createElement('div');
+    div.style.width = '200px';
+    div.style.height = '200px';
+    div.style.backgroundColor = color2;
+    div.style.border = '10px solid black';
+    document.body.appendChild(div);
+};
+
+document.addEventListener('keydown', (event) => {
     switch (event.key) {
         case 'a':
-            color = 'pink';
+            color1 = 'pink';
             break;
         case 's':
-            color = 'orange';
+            color1 = 'orange';
             break;
         case 'd':
-            color = 'skyblue';
+            color1 = 'skyblue';
             break;
-        default:
-            color = 'white';
+        case 'q':
+            color2 = 'purple';
+            createDiv(color2);
+            break;
+        case 'w':
+            color2 = 'gray';
+            createDiv(color2);
+            break;
+        case 'e':
+            color2 = 'brown';
+            createDiv(color2);
             break;
     }
 
-    document.getElementById('key').style.backgroundColor = color;
+    document.getElementById('key').style.backgroundColor = color1;
 });
